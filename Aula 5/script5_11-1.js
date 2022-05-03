5cf762c3689bba03380947263bf818afe83ee1c4
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-03 20:09:09 -0300

    Conteúdo da aula 5

--- Aula 5/script5_11-1.js
+++ Aula 5/script5_11-1.js
@@ -1,6 +1,19 @@
 var i, j;
-for (i=0; i<3; i++){ //externo, exemplo hora
- for (j=0; j<4; j++){ //interno, exemplo minuto
+for (i=0; i<2; i++){ //externo, exemplo hora
+ for (j=0; j<3; j++){ //interno, exemplo minuto
     document.write(i+" "+j+"<br>");
- }
+  }
 }
+
+/* loop aninhado
+para cada 1 vez que executa o externo, executa todas as vezes o interno
+exemplo: i externo e j interno
+o i espera o j acabar para continuar
+i  j
+0  0
+0  1
+0  2
+1  0
+1  1
+1  2
+*/
