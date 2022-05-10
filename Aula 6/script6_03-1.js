b4ee978f43b7d9bb15d13847dd12594bacfaeba2
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-10 20:55:23 -0300

    3 tipos de array

--- Aula 6/script6_03-1.js
+++ Aula 6/script6_03-1.js
@@ -1,6 +1,6 @@
 //Criação de variáveis tipo array de 3 maneiras diferentes
 
-var nota=[10,8.5,7.75,9.5]; //variável do tipo array com alocação dos valores na horizontal separados por vírgulas
+var nota=[10,8.5,7.75,9.5]; //MAIS UTILIZADO. Variável do tipo array com alocação dos valores na horizontal
 
 var num=[]; //variável do tipo array sem ter alocação de nenhum valor, e as atribuições são colocadas na sequência
 num[0]=120; //valores atribuídos de maneira individual
@@ -9,7 +9,7 @@ num[2]=2020;
 
 var nome=new Array("Jose","Paula","Carla"); //new array: referência de criação de objeto, não é tão utilizado
 
-nota=[]; //acabou de apagar o conteúdo de uma variável array
+nota=[]; //apagar o conteúdo de determinada variável array
 document.querySelector("#dv1").innerHTML=nota+"<br>"+num;
 document.querySelector(".teste").innerHTML=nome;
 /*
