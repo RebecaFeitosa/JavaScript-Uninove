9635777959df011b09a584ead95af9e54d526788
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-10 16:37:30 -0300

    exibindo apenas 1 valor da variável array

---
+++ Aula 6/script6_01-1.js
@@ -0,0 +1,4 @@
+//Posições     0    1     2
+var valores=["SP", 8.5, 2022];
+document.querySelector("#dv1").innerHTML=valores;   //exibir apenas um valor da variável array
+document.querySelector(".teste").innerHTML=valores[0]+valores[2];   //exibir mais de 1 valor da variável array
