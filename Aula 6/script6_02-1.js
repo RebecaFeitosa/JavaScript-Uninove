32db9c83d9ad09023a84748bf827fbdc872e1e25
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-10 16:48:38 -0300

    desafio usando comandos novos de saída

---
+++ Aula 6/script6_02-1.js
@@ -0,0 +1,16 @@
+var x=[2020, 2, "Hoje"];
+x[0]++;
+x[1]=x[0]%2;
+x[2]+=" em dia";
+
+
+/* 
+Desafios:
+1-x[0] e x[1] saída na área laranja
+2- x[2] saída na área aqua
+
+Comando de saída antigo:
+document.write(x[0]+"<br>");
+document.write(x[1]+"<br>");
+document.write(x[2]+"<br>");
+*/
