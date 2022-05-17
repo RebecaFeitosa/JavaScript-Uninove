f0a7e94b419f79b199f07f6f20afda14f8691f3e
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-17 20:16:30 -0300

    aprendendo propriedade length

--- Aula 6/script6_05-2.js
+++ Aula 6/script6_05-2.js
@@ -3,7 +3,9 @@ var cidade=["São Paulo", "Campinas", "Sorocaba", "Guarulhos", "Pedreira", "Jagu
 
 document.querySelector("h1").innerHTML=cidade[2]+" - "+cidade[3];
 
+document.querySelector("#dv1").innerHTML="A variável cidade tem "+cidade.length+" valores";  //nome_da_variavél_tipoarray.length traz a quantidade de valores na variável array
 
+document.querySelector("h2").innerHTML+=" - "+cidade[5];
 
 /* Desafios:
 1- Mudar o conteúdo da tag h1, por Soroacaba - Guarulhos
@@ -12,6 +14,8 @@ solução: document.querySelector("h1").innerHTML=cidade[2]+" - "+cidade[3];
 2- Mudar o conteúdo da tag div id="dv1" para mostrar a mensagem:
 "A variável cidade tem 6 valores" o número tem que ser baseado na quantidade 
 real de valores da var cidade tipo array
-solução: 
+solução: document.querySelector("#dv1").innerHTML="A variável cidade tem "+cidade.length+" valores";
 
+3- Incluir o último valor da var cidade na tag h2
+solução: 
 */
