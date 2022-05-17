b4bdeba17e004276ed3939b46c19086d2f7ddd0d
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-17 20:04:37 -0300

    desafios usando arrays

---
+++ Aula 6/script6_05-2.js
@@ -0,0 +1,17 @@
+//              0            1         2           3               4           5
+var cidade=["São Paulo", "Campinas", "Sorocaba", "Guarulhos", "Pedreira", "Jaguariuna"]
+
+document.querySelector("h1").innerHTML=cidade[2]+" - "+cidade[3];
+
+
+
+/* Desafios:
+1- Mudar o conteúdo da tag h1, por Soroacaba - Guarulhos
+solução: document.querySelector("h1").innerHTML=cidade[2]+" - "+cidade[3];
+
+2- Mudar o conteúdo da tag div id="dv1" para mostrar a mensagem:
+"A variável cidade tem 6 valores" o número tem que ser baseado na quantidade 
+real de valores da var cidade tipo array
+solução: 
+
+*/
