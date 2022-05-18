297d3d9edec52a7f22b0ad1b2532ca124e31a8c3
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-17 21:09:20 -0300

    Desafio emojis 1

---
+++ Aula 6/script6_desafio1.js
@@ -0,0 +1,11 @@
+for(var i=9193;i<9211;i++){
+   document.querySelector("#dv1").innerHTML+="&#"+i+" ";
+}
+
+
+
+/*
+Desafio 1:
+Criar um looping de: 9193 até: 9210
+A saída mostrar os emojis dos valores da faixa acima
+*/
