3bc6316a7a8df29c0ada8cad7ae9be0c6de5ffe9
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-17 21:26:50 -0300

    Desafio 2

---
+++ Aula 6/script6_desafio2.js
@@ -0,0 +1,11 @@
+var signo=["Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem", "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"]
+
+for (var i=9800; i<9812; i++){
+    document.querySelector(".teste").innerHTML+="&#9800 - "+signo[0];
+}
+
+/*
+Desafio 2:
+De: 9800 até: 9811
+Mostrar o Emoji e o signo ("string")
+*/
