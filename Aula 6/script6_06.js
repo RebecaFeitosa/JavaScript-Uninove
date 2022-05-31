c3eaf4fe1fceda9f85ba3a9179d4cf4f57ed1ecc
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-31 20:09:10 -0300

    Método toString

--- Aula 6/script6_06.js
+++ Aula 6/script6_06.js
@@ -7,7 +7,7 @@ document.querySelector("#dv").innerHTML=junto;
 /*
 Método toString: converte um array em 
 uma string com valores separados por vírgula, ou seja,
-pega todos os valores de determinada var array, 
-direciona para uma var string, carrega todos esses valores
-e imprime todos
+independente do tipo do valor, toString pega todos os valores 
+de determinada var array, direciona para uma var string, 
+carrega e imprime todos esses valores.
 */  
