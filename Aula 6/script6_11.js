d2004a3e157861c0ec96b21ad158f5ae2f52c2c6
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-05-31 21:33:42 -0300

    Métodos Sort e Reverse

---
+++ Aula 6/script6_11.js
@@ -0,0 +1,24 @@
+const id_dv=document.querySelector("#dv");
+const class_teste=document.querySelector(".teste");
+const id_nome=document.querySelector("#nome");
+const tag_h2=document.querySelector("h2");
+
+var uf=["SP", "RJ", "AC", "TO", "MG"];
+
+id_dv.innerHTML=uf;
+uf.sort();//ordenação crescente
+
+class_teste.innerHTML=uf;
+uf.reverse();//inverter o array
+
+id_nome.value=uf;
+
+/*
+Sort: ordenação crescente, classificar em ordem alfabética (A-Z).
+
+Reverse: reverter, inverte os valores da array, de trás para frente.
+
+
+Reverse dica: Se ordenar antes(sort) aparecerá em ordem Z-A, 
+se não usara o sort antes ele ira inverter o array
+*/
