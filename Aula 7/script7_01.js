a7574af03a68f7714db07d0813ff8df367ddac74
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-06-14 20:27:42 -0300

    Função

---
+++ Aula 7/script7_01.js
@@ -0,0 +1,13 @@
+/* -----------------------------------------
+function boas_vindas()
+Objetivo: Enviar mensagem de boas vindas
+Data de criação: 16/05/2021
+Data de alteração: 14/06/2022
+Autor: Nome do autor
+-------------------------------------------- */
+
+function boas_vindas() {
+    document.write("Seja bem vindo(a)");
+}//criação
+
+boas_vindas();//executa
