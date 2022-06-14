f7eb8d0e2838eaa68901f35783fac8ada6da5c30
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-06-14 20:39:05 -0300

    Criação de funções

--- Aula 7/script7_01.js
+++ Aula 7/script7_01.js
@@ -6,8 +6,21 @@ Data de alteração: 14/06/2022
 Autor: Nome do autor
 -------------------------------------------- */
 
-function boas_vindas() {
-    document.write("Seja bem vindo(a)");
-}//criação
+function boas_vindas(pessoa) {
+    document.write("Seja bem vindo(a): "+pessoa+"!");
+    document.write("<hr>")
+}//criação, ou, construir, ou, declara
 
-boas_vindas();//executa
+function negrito(txt){
+    document.write("<b>"+txt+"</b>");
+}
+
+function titulo2(tit){
+    document.write("<h2>"+tit+"</h2>");
+}
+
+
+titulo2("Título 2");
+boas_vindas("Jubileu");//executa, ou, chamada da função
+negrito("Esse texto está em negrito");
+titulo2("Último título 2");
