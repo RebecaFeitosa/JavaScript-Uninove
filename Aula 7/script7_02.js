7b1ca1cfe2a6c21735d1574332af7f51a3161c86
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-06-14 21:06:56 -0300

    Criação funções

--- Aula 7/script7_02.js
+++ Aula 7/script7_02.js
@@ -1,12 +1,17 @@
 function media(nota1, nota2, nota3) {
     return (nota1+nota2+nota3)/3;
+    //return "Eita!!!";
 } 
-//usa o return dentro da função, não usa comando de saída
+/*
+usa o return dentro da função, não usa comando de saída
+o return tem que devolver o valor para alguém
+*/
 
-var num=media (8, 7, 9); 
-document.write ("Num = "+num); 
+var num = media (8, 7, 9); 
+document.write ("Num = "+num+"<br>"); 
+document.write ("Média Final: "+media(9, 7, 9));
 
 /*
-valores da nota1, 2 e 3 atribuídos dentro do ()
+valores das notas atribuídos dentro do ()
 o comando de saída que eu preferir será usado fora da função
 */
