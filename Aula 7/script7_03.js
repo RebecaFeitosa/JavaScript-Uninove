7b963892610a0507005c69ced1450fcf9dc10137
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-06-14 21:47:27 -0300

    Calculadora com funções

--- Aula 7/script7_03.js
+++ Aula 7/script7_03.js
@@ -1,7 +1,25 @@
 function calc(n1, n2,op){
-    return n1+n2;
+    if(op=="+"){
+        return n1+n2;
+    }
+    else if(op=="-"){
+        return n1-n2;
+    }
+    else if(op=="*"){
+        return n1*n2;
+    }
+    else if (op=="/"){
+        return n1/n2;
+    }
+    else if(op=="%"){
+        return n1%n2;
+    }
+    else{
+        return "Operação inválida";
+    }
 }
 
 var num1=parseFloat(prompt("Digite um número"));
 var num2=parseFloat(prompt("Digite outro número"));
-document.write("1° Número: "+num1+"<br> 2° Número: "+num2+"<br> Cálculo: "+calc(num1,num2,"+"));
+var operador=prompt("Digite o operador");
+document.write("1° Número: "+num1+"<br> 2° Número: "+num2+"<br> Cálculo: "+calc(num1,num2,operador));
